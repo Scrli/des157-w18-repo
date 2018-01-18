323cc5b5330de28eac48d66bb82c9c14007d64aa
sijiali <scrli@ucdavis.edu>
2018-01-18 00:28:40 -0800

    init commit, add first homework

---
+++ portal_homepage/script.js
@@ -0,0 +1,20 @@
+// p5 file
+console.log("this is script.js comment");
+
+function setup(){
+  var myCanvas = createCanvas(800,200);
+  myCanvas.parent('mySketch');
+  background(0);
+  stroke(0);
+}
+
+function draw(){
+  if (mouseIsPressed){
+    fill(244,56,56);
+
+  }else {
+    fill(255);
+  }
+
+ellipse(mouseX,mouseY,70,50,40,20)
+}
